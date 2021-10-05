e546a1f14728be84bdfdeabcff427a551de7a687
Yuqing Wu <ywu20@stuy.edu>
2021-10-05 17:28:17 -0400

    initial commit, makefile compile test

---
+++ Makefile
@@ -0,0 +1,6 @@
+all:	main.o	mystring.o
+	gcc	-o	main	main.o	mystring.o
+main.o:	main.c	mystring.h
+	gcc	-c	main.c
+mystring.o:	mystring.c	mystring.h
+	gcc	-c	mystring.c
---
+++ mystring.c
@@ -0,0 +1,25 @@
+//strlen [ int mystrlen( char *s ) ]
+int mystrlen(char*s){
+  return 0;
+}
+//strcpy/strncpy (choose one) [ char * mystrcpy( char *dest, char *source ) char * mystrncpy( char *dest, char *source, int n) ]
+char* mystrncpy(char *dest, char *source, int n){
+  char * temp;
+  return temp;
+}
+//strcat/strncat (choose one) [ char * mystrcat( char *dest, char *source ) char * mystrncat( char *dest, char *source, int n) ]
+char * mystrcat( char *dest, char *source){
+  char * temp;
+  return temp;
+}
+
+//strcmp [ int mystrcmp( char *s1, char *s2 ) ]
+int mystrcmp( char *s1, char *s2 ){
+  return 0;
+}
+//    Your version does not need to return the same exact value as the system version, as long as it returns -, + or 0 when it should.
+//strchr [ char * mystrchr( char *s, char c ) ]
+char * mystrchr( char *s, char c ){
+  char * temp;
+  return temp;
+}
